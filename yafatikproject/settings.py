74a4d5ffe38c01b58993e8edd7407661b6780953
abdulrahmanHussein <100815636+abdulrahmanHussein@users.noreply.github.com>
2023-08-19 00:10:03 +0800

    Update settings.py

--- yafatikproject/settings.py
+++ yafatikproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8s$4xfg=p*6kycxj6ncmms+06wem$d#e8layw)k@y2k25(d0bf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.yafatik.com',yafatik.com','dijango.yafatik.com','www.dijango.yafatik.com','ec2-3-0-16-8.ap-southeast-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['www.yafatik.com','yafatik.com','dijango.yafatik.com','www.dijango.yafatik.com','ec2-3-0-16-8.ap-southeast-1.compute.amazonaws.com']
 
 
 
